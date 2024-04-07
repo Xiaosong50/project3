43e0695a27d7d5b47f9cd866c4c2718aae7d592f
Xiaosong50 <xlu50@hawk.iit.edu>
2024-04-06 19:11:00 -0500

    add layers

--- js/map.js
+++ js/map.js
@@ -6,7 +6,7 @@ function initMap()
     zoom: 10
   });
   
-  var locationButton=document.getElementById("button");
+  var locationButton=document.getElementById("location");
   locationButton.addEventListener("click",()=> {
     if (navigator.geolocation){
       navigator.geolocation.getCurrentPosition(
@@ -18,11 +18,50 @@ function initMap()
           map.setCenter(pos);
           var marker=new google.maps.Marker({
             position: pos,
-            map:map
+            map:map,
           });
           map.setZoom(17);
         }
       )
+    }
+  });
+  
+  var TrafficButton=document.getElementById("trafficLayer");
+  var TrafficRecord = 0;
+  var TransitButton=document.getElementById("transitLayer");
+  var TransitRecord = 0;
+  var BicyclingButton=document.getElementById("bikeLayer");
+  var BicyclingRecord = 0;
+  const trafficLayer = new google.maps.TrafficLayer();
+  const transitLayer = new google.maps.TransitLayer();
+  const bikeLayer = new google.maps.BicyclingLayer();
+  TrafficButton.addEventListener("click",()=>{
+    if(TrafficRecord==0){
+      trafficLayer.setMap(map);
+      TrafficRecord=1;
+    } else{
+      trafficLayer.setMap(null);
+      TrafficRecord=0;
+    }
+  });
+  
+  TransitButton.addEventListener("click",()=>{
+    if(TransitRecord==0){
+      transitLayer.setMap(map);
+      TransitRecord=1;
+    } else{
+      transitLayer.setMap(null);
+      TransitRecord=0;
+    }
+  });
+  
+  BicyclingButton.addEventListener("click",()=>{
+    if(BicyclingRecord==0){
+      bikeLayer.setMap(map);
+      BicyclingRecord=1;
+    } else{
+      bikeLayer.setMap(null);
+      BicyclingRecord=0;
     }
   });
   
